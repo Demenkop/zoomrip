d92a8509068759789520daf5eb60a8b936f3c2a2
Nikita <isfellop@gmail.com>
2020-03-31 23:09:10 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,4 +3,4 @@ import re
 
 auth_re = re.compile(r'config.auth = "(.*)";')
 ts_re = re.compile(r"config.ts = '(.*)';")
-url_re = re.compile(r"http.*://.*\.zoom\.us/j/(\d*)(\?pwd=(.*)|)")
+url_re = re.compile(r"http.*://.*(\.|)zoom\.us/j/(\d*)(\?pwd=(.*)|)")
